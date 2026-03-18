b5bf843816ae3be3cd13c9ec54611e7a5a8f6880
finda <72123712+finda9531@users.noreply.github.com>
2026-03-18 20:21:43 +0900

    metier.Designer.cs を更新

--- metier/metier.Designer.cs
+++ metier/metier.Designer.cs
@@ -1,17 +1,12 @@
-﻿namespace Metier
+namespace Metier
 {
     // クラス名が Metier になっていること、partial であることを確認
     partial class Metier
     {
-        /// <summary>
-        /// 必要なデザイナー変数です。
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// 使用中のリソースをすべてクリーンアップします。
-        /// </summary>
-        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,10 +18,7 @@
 
         #region Windows フォーム デザイナーで生成されたコード
 
-        /// <summary>
-        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
-        /// コード エディターで変更しないでください。
-        /// </summary>
+       
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
